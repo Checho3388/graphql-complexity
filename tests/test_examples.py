f234efdb1977882b2ceb3368c142416906173f65
Checho3388 <ezequiel.grondona@gmail.com>
2024-02-19 16:58:02 -0300

    test: Add unit tests for examples in Readme.md

---
+++ tests/test_examples.py
@@ -0,0 +1,68 @@
+from graphql import parse, visit
+
+from graphql_complexity.estimators import DirectivesEstimator
+from graphql_complexity.estimators import SimpleEstimator
+from graphql_complexity.visitor import ComplexityVisitor
+
+
+def _evaluate_complexity_with_simple_estimator(
+    query: str, field_complexity=1, multiplier=1
+):
+    ast = parse(query)
+    estimator = SimpleEstimator(field_complexity, multiplier)
+    visitor = ComplexityVisitor(estimator=estimator)
+    visit(ast, visitor)
+
+    return visitor.evaluate()
+
+
+def test_root_fields_complexity_is_added():
+    query = """
+        query {
+            user {
+                name
+                email
+            }
+        }
+    """
+
+    complexity = _evaluate_complexity_with_simple_estimator(query, 1, 2)
+
+    assert complexity == 10
+
+
+def _evaluate_complexity_with_directives_estimator(
+    query: str,
+    schema: str,
+):
+    ast = parse(query)
+    estimator = DirectivesEstimator(schema)
+    visitor = ComplexityVisitor(estimator=estimator)
+    visit(ast, visitor)
+
+    return visitor.evaluate()
+
+
+def test_simple_query_with_directive_estimator():
+    schema = """directive @complexity(
+      value: Int!
+    ) on FIELD_DEFINITION
+
+    type Query {
+      oneField: String @complexity(value: 5)
+      otherField: String @complexity(value: 1)
+      withoutDirective: String
+    }
+    """
+
+    query = """
+        query Something {
+            oneField
+            otherField
+            withoutDirective
+        }
+    """
+
+    complexity = _evaluate_complexity_with_directives_estimator(query, schema)
+
+    assert complexity == 7
