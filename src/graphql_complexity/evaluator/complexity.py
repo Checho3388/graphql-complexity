8ba7f08c38f701404f8f73c68d9e71d412b0be24
Checho3388 <ezequiel.grondona@gmail.com>
2024-03-12 10:41:11 -0300

    refactor: Unify complexity functions

--- src/graphql_complexity/evaluator/complexity.py
+++ src/graphql_complexity/evaluator/complexity.py
@@ -1,20 +1,16 @@
 from graphql import GraphQLSchema, TypeInfo, TypeInfoVisitor, parse, visit
 
-from ..estimators import ComplexityEstimator
 from .visitor import ComplexityVisitor
+from ..config import Config
+from ..estimators import ComplexityEstimator
 
 
-def get_complexity(query: str, schema: GraphQLSchema, estimator: ComplexityEstimator) -> int:
+def get_complexity(query: str, schema: GraphQLSchema, estimator: ComplexityEstimator, config: Config = None) -> int:
     """Calculate the complexity of a query using the provided estimator."""
     ast = parse(query)
-    return get_ast_complexity(ast, schema=schema, estimator=estimator)
-
-
-def get_ast_complexity(ast, schema: GraphQLSchema, estimator: ComplexityEstimator) -> int:
-    """Calculate the complexity of a query using the provided estimator."""
     type_info = TypeInfo(schema)
 
-    visitor = ComplexityVisitor(estimator=estimator, type_info=type_info)
+    visitor = ComplexityVisitor(estimator=estimator, type_info=type_info, config=config)
     visit(ast, TypeInfoVisitor(type_info, visitor))
 
     return visitor.evaluate()
