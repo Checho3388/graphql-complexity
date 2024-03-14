6502eb6bd1509b435fbaae19491a96dfad9ac380
Checho3388 <ezequiel.grondona@gmail.com>
2024-03-14 16:22:46 -0300

    refactor: Killed some mutants using mutmut. Added UTs and also removed unnecessary conditions (not tested, not required) in code.

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -68,6 +68,41 @@ def test_describes_fragment_spread():
 \tfragmentName (FragmentSpreadNode) = 1"""
 
 
+def test_describes_complex_query():
+    query = """query {
+        droid {
+            friends {
+                name
+            }
+            appearsIn
+        }
+    }"""
+
+    tree = _build_complexity_tree(query)
+
+    assert tree.describe() == """root (RootNode) = 4
+\tdroid (Field) = 4
+\t\tfriends (ListField) = 2
+\t\t\tname (Field) = 1
+\t\tappearsIn (ListField) = 1"""
+
+
+def test_typename_is_built_as_meta():
+    query = """query {
+        droid {
+            __typename
+            id
+        }
+    }"""
+
+    tree = _build_complexity_tree(query)
+
+    assert tree.describe() == """root (RootNode) = 2
+\tdroid (Field) = 2
+\t\t__typename (MetaField) = 0
+\t\tid (Field) = 1"""
+
+
 def test_complexity_node_can_not_be_evaluated():
     node = ComplexityNode(
         name="root"
@@ -75,3 +110,27 @@ def test_complexity_node_can_not_be_evaluated():
 
     with pytest.raises(NotImplementedError):
         node.evaluate()
+
+
+def test_complexity_node_have_no_parent_until_they_are_linked():
+    node_parent = ComplexityNode(name="parent")
+    node_child = ComplexityNode(name="child")
+
+    assert node_parent.parent is None
+    assert node_child.parent is None
+    assert node_parent.children == []
+    assert node_child.children == []
+
+    node_parent.add_child(node_child)
+
+    assert node_parent.parent is None
+    assert node_child.parent is node_parent
+    assert node_parent.children == [node_child]
+    assert node_child.children == []
+
+
+def test_children_should_be_complexity_nodes():
+    node = ComplexityNode(name="root")
+
+    with pytest.raises(TypeError, match="^Children must be ComplexityNode instances, got <class 'str'>$"):
+        node.add_child("not a node")
